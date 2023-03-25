d25036d0e5ceccfc8fca983e15638bc537e61d6e
Martin de La Gorce <martin.delagorce@gmail.com>
2023-03-25 19:03:12 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+libname="postponed"
+setup(
+    name = libname,
+    version="0.1",
+    packages=['postponed'],
+    ext_modules=[],
+    include_dirs=[],
+    install_requires=[]
+)
+
